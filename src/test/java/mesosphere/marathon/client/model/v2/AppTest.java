2bf92e3b1d75a4f56ef7a635afe47805b3212d86
Colin C <colin@mesosphere.com>
2016-03-01 15:53:15 -0800

    Add More Fields
    This commit adds more fields and tests.

--- src/test/java/mesosphere/marathon/client/model/v2/AppTest.java
+++ src/test/java/mesosphere/marathon/client/model/v2/AppTest.java
@@ -1,10 +1,19 @@
 package mesosphere.marathon.client.model.v2;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class AppTest {
+    private App app;
+
+    @Before
+    public void setUp() {
+        this.app = new App();
+    }
+
     /**
      * Test that roles can be added individually and duplicate roles
      * do not cause an issue.
@@ -13,15 +22,41 @@ public class AppTest {
      */
     @Test
     public void testAddAcceptedResourceRole() throws Exception {
-        final App app = new App();
-        app.addAcceptedResourceRole("arole");
+        final String firstRole  = "arole";
+        final String secondRole = "another";
+
+        app.addAcceptedResourceRole(firstRole);
         assertTrue("Added role exists",
-                app.getAcceptedResourceRoles().contains("arole"));
-        app.addAcceptedResourceRole("another");
+                app.getAcceptedResourceRoles().contains(firstRole));
+        app.addAcceptedResourceRole(secondRole);
         assertTrue("Second added role exists",
-                app.getAcceptedResourceRoles().contains("another"));
-        app.addAcceptedResourceRole("arole");
+                app.getAcceptedResourceRoles().contains(secondRole));
+        app.addAcceptedResourceRole(firstRole);
         assertTrue("Duplicate role exists",
-                app.getAcceptedResourceRoles().contains("arole"));
+                app.getAcceptedResourceRoles().contains(firstRole));
+    }
+
+    @Test
+    public void testAddDependency() throws Exception {
+        final String firstDep  = "adep";
+        final String secondDep = "another";
+
+        app.addDependency(firstDep);
+        assertTrue("Added dependency exists",
+                app.getDependencies().contains(firstDep));
+        app.addDependency(secondDep);
+        assertTrue("Second dependency exists",
+                app.getDependencies().contains(secondDep));
+        app.addDependency(firstDep);
+        assertTrue("Duplicate dependency exists and no errors",
+                app.getDependencies().contains(firstDep));
+    }
+
+    @Test
+    public void testBackoffFactor() throws Exception {
+        app.setBackoffFactor(0.1);
+        assertEquals(app.getBackoffFactor(), Double.valueOf(0.1));
+        app.setBackoffFactor(1d);
+        assertEquals(app.getBackoffFactor(), Double.valueOf(1.0));
     }
 }
