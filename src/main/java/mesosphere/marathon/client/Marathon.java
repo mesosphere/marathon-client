5d3e2af89ae76fbff279a3bb10e300bb60ea32a7
Carlos Sanchez <carlos@apache.org>
2016-04-20 11:28:40 +0200

    Upgrade feign and gson
    Change no longer supported @Named to @Param

--- src/main/java/mesosphere/marathon/client/Marathon.java
+++ src/main/java/mesosphere/marathon/client/Marathon.java
@@ -2,8 +2,6 @@ package mesosphere.marathon.client;
 
 import java.util.List;
 
-import javax.inject.Named;
-
 import mesosphere.marathon.client.model.v2.App;
 import mesosphere.marathon.client.model.v2.DeleteAppTaskResponse;
 import mesosphere.marathon.client.model.v2.DeleteAppTasksResponse;
@@ -18,6 +16,8 @@ import mesosphere.marathon.client.model.v2.GetTasksResponse;
 import mesosphere.marathon.client.model.v2.Group;
 import mesosphere.marathon.client.model.v2.Result;
 import mesosphere.marathon.client.utils.MarathonException;
+
+import feign.Param;
 import feign.RequestLine;
 
 public interface Marathon {
@@ -26,10 +26,10 @@ public interface Marathon {
 	GetAppsResponse getApps() throws MarathonException;
 
 	@RequestLine("GET /v2/apps/{id}")
-	GetAppResponse getApp(@Named("id") String id) throws MarathonException;
+	GetAppResponse getApp(@Param("id") String id) throws MarathonException;
 
 	@RequestLine("GET /v2/apps/{id}/tasks")
-	GetAppTasksResponse getAppTasks(@Named("id") String id) throws MarathonException;
+	GetAppTasksResponse getAppTasks(@Param("id") String id) throws MarathonException;
 
 	@RequestLine("GET /v2/tasks")
 	GetTasksResponse getTasks() throws MarathonException;
@@ -38,31 +38,31 @@ public interface Marathon {
 	App createApp(App app) throws MarathonException;
 
 	@RequestLine("PUT /v2/apps/{app_id}")
-	Result updateApp(@Named("app_id") String appId, App app) throws MarathonException;
+	Result updateApp(@Param("app_id") String appId, App app) throws MarathonException;
 
 	@RequestLine("POST /v2/apps/{id}/restart?force={force}")
-	void restartApp(@Named("id") String id,@Named("force") boolean force) throws MarathonException;
+	void restartApp(@Param("id") String id,@Param("force") boolean force) throws MarathonException;
 
 	@RequestLine("DELETE /v2/apps/{id}")
-	Result deleteApp(@Named("id") String id) throws MarathonException;
+	Result deleteApp(@Param("id") String id) throws MarathonException;
 
 	@RequestLine("DELETE /v2/apps/{app_id}/tasks?host={host}&scale={scale}")
-	DeleteAppTasksResponse deleteAppTasks(@Named("app_id") String appId,
-			@Named("host") String host, @Named("scale") String scale) throws MarathonException;
+	DeleteAppTasksResponse deleteAppTasks(@Param("app_id") String appId,
+			@Param("host") String host, @Param("scale") String scale) throws MarathonException;
 
 	@RequestLine("DELETE /v2/apps/{app_id}/tasks/{task_id}?scale={scale}")
-	DeleteAppTaskResponse deleteAppTask(@Named("app_id") String appId,
-			@Named("task_id") String taskId, @Named("scale") String scale) throws MarathonException;
+	DeleteAppTaskResponse deleteAppTask(@Param("app_id") String appId,
+			@Param("task_id") String taskId, @Param("scale") String scale) throws MarathonException;
 
     // Groups
 	@RequestLine("POST /v2/groups")
 	Result createGroup(Group group) throws MarathonException;
 	
 	@RequestLine("DELETE /v2/groups/{id}")
-	Result deleteGroup(@Named("id") String id) throws MarathonException;
+	Result deleteGroup(@Param("id") String id) throws MarathonException;
 	
 	@RequestLine("GET /v2/groups/{id}")
-	Group getGroup(@Named("id") String id) throws MarathonException;
+	Group getGroup(@Param("id") String id) throws MarathonException;
 
     // Tasks
 
@@ -71,18 +71,18 @@ public interface Marathon {
 	List<Deployment> getDeployments() throws MarathonException;
 	
 	@RequestLine("DELETE /v2/deployments/{deploymentId}")
-	void cancelDeploymentAndRollback(@Named("deploymentId") String id) throws MarathonException;
+	void cancelDeploymentAndRollback(@Param("deploymentId") String id) throws MarathonException;
 	
 	@RequestLine("DELETE /v2/deployments/{deploymentId}?force=true")
-	void cancelDeployment(@Named("deploymentId") String id) throws MarathonException;
+	void cancelDeployment(@Param("deploymentId") String id) throws MarathonException;
 
     // Event Subscriptions
 
     @RequestLine("POST /v2/eventSubscriptions?callbackUrl={url}")
-    public GetEventSubscriptionRegisterResponse register(@Named("url") String url) throws MarathonException;
+    public GetEventSubscriptionRegisterResponse register(@Param("url") String url) throws MarathonException;
 
     @RequestLine("DELETE /v2/eventSubscriptions?callbackUrl={url}")
-    public GetEventSubscriptionRegisterResponse unregister(@Named("url") String url) throws MarathonException;
+    public GetEventSubscriptionRegisterResponse unregister(@Param("url") String url) throws MarathonException;
 
     @RequestLine("GET /v2/eventSubscriptions")
     public GetEventSubscriptionsResponse subscriptions() throws MarathonException;
