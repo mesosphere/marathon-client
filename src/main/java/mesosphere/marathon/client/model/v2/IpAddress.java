64239a0b03aa2806a38223b5378c6c00e88b59e9
Colin C <colin@mesosphere.com>
2017-05-15 13:56:00 -0700

    Add labels to IpAddress

--- src/main/java/mesosphere/marathon/client/model/v2/IpAddress.java
+++ src/main/java/mesosphere/marathon/client/model/v2/IpAddress.java
@@ -3,12 +3,15 @@ package mesosphere.marathon.client.model.v2;
 import mesosphere.client.common.ModelUtils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class IpAddress {
-    private IpDiscovery  discovery;
-    private List<String> groups;
-    private String       networkName;
+    private IpDiscovery         discovery;
+    private List<String>        groups;
+    private Map<String, Object> labels;
+    private String              networkName;
 
     public IpDiscovery getDiscovery() {
         return discovery;
@@ -29,6 +32,24 @@ public class IpAddress {
     public void addGroup(final String group) {
         if (this.groups == null) this.groups = new ArrayList<>(5);
         this.groups.add(group);
+    }
+
+    public Map<String, Object> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(Map<String, Object> labels) {
+        this.labels = labels;
+    }
+
+    public void addLabel(final String key, final Object value) {
+        if (key != null && key.trim().length() > 0 && value != null) {
+            if (this.labels == null) {
+                this.labels = new HashMap<>(5);
+            }
+
+            this.labels.put(key, value);
+        }
     }
 
     public String getNetworkName() {
